d731dbc21899444e2c69788de5587a0085a31d36
quentinfayolle34 <fayollequentin99@gmail.com>
2026-01-13 23:00:24 +0100

    build

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Home from './Home.tsx'
+// import Home from './Home.tsx'
 
-import TopTen from './jeux/TopTen.tsx'
-import DiceCube from './jeux/Treeman.tsx'
+// import TopTen from './jeux/TopTen.tsx'
+// import DiceCube from './jeux/Treeman.tsx'
 import DiceGame from './jeux/DiceGame.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <DiceGame /> */}
-    <TopTen />
+    <DiceGame />
+    {/* <TopTen /> */}
     {/* <Home /> */}
   </StrictMode>,
 )
